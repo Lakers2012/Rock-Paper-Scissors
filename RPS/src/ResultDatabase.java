b0867b36578937ad1484252d9fe73e7dc92da5b1
Lakers2012 <ereedley@yahoo.com>
2012-03-02 09:40:05 -0800

    Update RPS/src/ResultDatabase.java

--- RPS/src/ResultDatabase.java
+++ RPS/src/ResultDatabase.java
@@ -1,6 +1,44 @@
-public class ResultDatabase {
-    public Result add(Result r) {
-        return null;
+import java.util.*;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Danny
+ */
+public class ResultsDataBase {
+
+    private ArrayList<Result> results;
+    int t, p, c, s = 0;
+    public ResultsDataBase()
+    {
+        results = new ArrayList<Result>();
     }
-    // and other methods ...  this is just to make it compile
+
+    public boolean addResult(Result r)
+    {
+        int i = results.size();
+        results.add(r);
+        s = r.getOutcome();
+                if(s == 0)
+                    c++;
+                else if(s == 1)
+                    p++;
+                else
+                    t++;
+        if(results.size() > i)
+            return true;
+        else
+            return false;
+    }
+
+    public String getScore()
+    {
+        String str = "";
+        str = "Match result are: " + p + " Wins, " + c + " Losses, and " + t + " Ties";
+        return str;
+    }
+
 }
